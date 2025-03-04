c0871a4c374bfa0b4714cf35d661c506a5d732b2
Dylan Lara <dl870277@gmail.com>
2025-03-03 22:44:14 -0600

    el .env otra vez jodiendo

--- server/settings.py
+++ server/settings.py
@@ -154,9 +154,10 @@ CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 
 # Config cloudinary 
 
-CLOUDINARY_STORAGE = {
+""" CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+ """
