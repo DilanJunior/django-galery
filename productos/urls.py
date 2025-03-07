eaf679c117db5ccb988f95c4bb00c275da9d4c73
Dylan Lara <dl870277@gmail.com>
2025-03-07 02:00:48 -0600

    cambios #18

--- productos/urls.py
+++ productos/urls.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path
-from .views import Home, Home_redirect, test_redis_cache
+from .views import Home, Home_redirect
 
 urlpatterns = [ path('store/', Home, name='Home'),
                 path('', Home_redirect, name="Home_redirect"),
-                path('test-redis-cache/', test_redis_cache, name='test_redis_cache'),
+                
 ]
 
 if settings.DEBUG:
